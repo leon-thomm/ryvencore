1eb3232866fafb7c8547c728f79d9b37c8a734dc
Leon Thomm <l.thomm@mailbox.org>
2020-12-24 01:06:37 +0100

    Delete __init__.py

--- src/__init__.py
+++
@@ -1,6 +0,0 @@
-# from .GlobalAttributes import package_path
-# import os
-#
-# GlobalAttributes.package_path = os.path.normpath(os.path.dirname(os.path.abspath(__file__))+'/../')
-
-from . import ryvencore
