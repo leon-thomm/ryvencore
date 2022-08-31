e1059ac2a0049b64574be15657086e40097a93d3
leon-thomm <l.thomm@mailbox.org>
2022-08-31 19:12:24 +0800

    cython doc tweaks

--- setup_cython.py
+++ setup_cython.py
@@ -8,9 +8,9 @@ $ pip uninstall ryvencore
 The below instructions show how to compile the sources on your system. Note that you will need Cython and an according
 C compiler for this.
 
-$ python -m setup_cython build_ext --inplace
-$ python setup_cython.py sdist bdist_wheel
-$ pip install ./dist/ryvencore-<version>-<platform>.whl
+    $ python -m setup_cython build_ext --inplace
+    $ python setup_cython.py sdist bdist_wheel
+    $ pip install ./dist/ryvencore-<version>-<platform>.whl
 
 NOTE
 -   if you don't want to keep the source files (.py and compiled .c) in the installed package (only the compiled .so
@@ -18,10 +18,15 @@ NOTE
 
 To verify that the package successfully runs from the compiled C extension modules you can check whether
 the imported ryvencore package shows the __init__.so file and not __init__.py
-> import ryvencore as rc
-> print(rc)
+
+    $ python
+    > import ryvencore as rc
+    > print(rc)
+
 and to really be sure you can also manually remove all .py files in the installation directory
-(on linux: $ .../site-packages/ryvencore>find . -name "*.py" -type f -delete)
+
+    on Linux: $ .../site-packages/ryvencore> find . -name "*.py" -type f -delete
+
 and check whether you can successfully load the package from Python.
 """
 
